e14bd4252d53681477004d67a849a909ae8568a0
Wesley Moore <wes@wezm.net>
2018-01-21 21:20:04 +1100

    Add BSD support to build script

--- build.rs
+++ build.rs
@@ -13,7 +13,7 @@ fn main() {
 	let target = env::var("TARGET").unwrap();
 	if target.contains("windows") {
 		build.define("WEBVIEW_WINAPI", None);
-	} else if target.contains("linux") {
+	} else if target.contains("linux") || target.contains("bsd") {
 		build.define("WEBVIEW_GTK", None);
 	} else if target.contains("apple") {
 		build.define("WEBVIEW_COCOA", None);
