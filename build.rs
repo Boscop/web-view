b0d928789b359712ab5348049dbffa321891cf82
Boscop <kingboscop@gmail.com>
2018-01-22 11:03:28 +0100

    fix travis build by lowering required webkit version

--- build.rs
+++ build.rs
@@ -15,7 +15,7 @@ fn main() {
 	if target.contains("windows") {
 		build.define("WEBVIEW_WINAPI", None);
 	} else if target.contains("linux") || target.contains("bsd") {
-		let webkit = pkg_config::Config::new().atleast_version("2.16").probe("webkit2gtk-4.0").unwrap();
+		let webkit = pkg_config::Config::new().atleast_version("2.8").probe("webkit2gtk-4.0").unwrap();
 
 		for path in webkit.include_paths {
 			build.include(path);
