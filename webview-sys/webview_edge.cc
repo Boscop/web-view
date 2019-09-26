44c614d68252b856924b9e11390524c029227b00
zxey <r.hozak@seznam.cz>
2019-09-26 22:49:30 +0200

    Add remaining missing function stubs

--- webview-sys/webview_edge.cc
+++ webview-sys/webview_edge.cc
@@ -27,6 +27,27 @@ void webview_exit(webview_t w) {
 	webview_terminate(w);
 }
 
+void webview_set_color(webview_t w, uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+{
+	// TODO
+}
+
+void webview_dialog(webview_t w, int dlgtype, int flags, const char *title, const char *arg, char *result, size_t resultsz)
+{
+	// TODO
+}
+
+void webview_set_fullscreen(webview_t w, int fullscreen)
+{
+	// TODO
+}
+
+int webview_inject_css(webview_t w, const char *css)
+{
+	// TODO
+	return 0;
+}
+
 #ifdef __cplusplus
 }
 #endif
