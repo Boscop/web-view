4b5f9198625221a8a68779bf76822cb4e448ec91
zxey <r.hozak@seznam.cz>
2019-09-28 23:44:00 +0200

    Fix resizable flag not working and fix initial window size
    Window now respects whether resizable was set or not, it was ignore previously.
    We can also set initial width and height of the window now, previously you would set it after window creation which resulted in window jumping
    from its initial position, that is fixed now.

--- webview-sys/webview_edge.cc
+++ webview-sys/webview_edge.cc
@@ -9,10 +9,9 @@ void wrapper_webview_free(webview_t w) {
 }
 
 webview_t wrapper_webview_new(const char* title, const char* url, int width, int height, int resizable, int debug, webview_external_invoke_cb_t external_invoke_cb, void* userdata) {
-	webview_t w = webview_create(external_invoke_cb, debug, nullptr);
+	webview_t w = webview_create(external_invoke_cb, width, height, resizable, debug);
 	webview_set_userdata(w, userdata);
 	webview_set_title(w, title);
-	webview_set_bounds(w, 50, 50, width, height, 0);
 	webview_navigate(w, url);
 	return w;
 }
