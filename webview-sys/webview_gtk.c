471d9aa7a4fc0faaa2993e4929ee01e3b5d6c150
zxey <r.hozak@seznam.cz>
2019-10-07 23:52:36 +0200

    Rename wrapper functions to webview functions

--- webview-sys/webview_gtk.c
+++ webview-sys/webview_gtk.c
@@ -27,16 +27,16 @@ struct gtk_webview {
   void *userdata;
 };
 
-WEBVIEW_API void wrapper_webview_free(webview_t w) {
+WEBVIEW_API void webview_free(webview_t w) {
 	free(w);
 }
 
-WEBVIEW_API void* wrapper_webview_get_userdata(webview_t w) {
+WEBVIEW_API void* webview_get_user_data(webview_t w) {
   struct gtk_webview *wv = (struct webview *)w;
 	return wv->userdata;
 }
 
-WEBVIEW_API webview_t wrapper_webview_new(const char* title, const char* url, int width, int height, int resizable, int debug, webview_external_invoke_cb_t external_invoke_cb, void* userdata) {
+WEBVIEW_API webview_t webview_new(const char* title, const char* url, int width, int height, int resizable, int debug, webview_external_invoke_cb_t external_invoke_cb, void* userdata) {
 	struct gtk_webview* w = (struct gtk_webview*)calloc(1, sizeof(*w));
 	w->width = width;
 	w->height = height;
@@ -47,7 +47,7 @@ WEBVIEW_API webview_t wrapper_webview_new(const char* title, const char* url, in
 	w->external_invoke_cb = external_invoke_cb;
 	w->userdata = userdata;
 	if (webview_init(w) != 0) {
-		wrapper_webview_free(w);
+		webview_free(w);
 		return NULL;
 	}
 	return w;
