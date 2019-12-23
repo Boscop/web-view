dea84e645619d7d655d38de94c4afea867116b0f
zxey <r.hozak@seznam.cz>
2019-12-23 01:37:24 +0100

    Add two minimal examples
    fullscreen - shows two buttons for entering and exiting fullscreen mode
    title_change - show text input that changes windows title as you type

---
+++ examples/fullscreen.rs
@@ -0,0 +1,33 @@
+extern crate web_view;
+
+use web_view::*;
+
+fn main() {
+    web_view::builder()
+        .title("Change title with input")
+        .content(Content::Html(HTML))
+        .size(800, 100)
+        .resizable(true)
+        .debug(true)
+        .user_data("")
+        .invoke_handler(|webview, arg| {
+            match arg {
+                "enter" => webview.set_fullscreen(true),
+                "exit" => webview.set_fullscreen(false),
+                _ => ()
+            }
+            Ok(())
+        })
+        .run()
+        .unwrap();
+}
+
+const HTML: &str = r#"
+<!doctype html>
+<html>
+	<body>
+        <button onclick="external.invoke('enter')">enter fullscreen</button>
+        <button onclick="external.invoke('exit')">exit fullscreen</button>
+	</body>
+</html>
+"#;
---
+++ examples/title_change.rs
@@ -0,0 +1,34 @@
+extern crate web_view;
+
+use web_view::*;
+
+fn main() {
+    web_view::builder()
+        .title("Change title with input")
+        .content(Content::Html(HTML))
+        .size(800, 100)
+        .resizable(true)
+        .debug(true)
+        .user_data("")
+        .invoke_handler(|webview, arg| {
+            webview.set_title(arg)
+        })
+        .run()
+        .unwrap();
+}
+
+const HTML: &str = r#"
+<!doctype html>
+<html>
+	<body>
+        <input id="title" type="text" placeholder="Title" style="width: 100%; padding: none; margin: none; font-size: large;"/>
+        <script type="text/javascript">
+            function updateTitle(e) {
+                external.invoke(e.target.value);
+            }
+            var el = document.getElementById("title");
+            el.addEventListener("input", updateTitle, false);
+		</script>
+	</body>
+</html>
+"#;
