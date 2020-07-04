c457ea9b3143bcfb35c53adec435dc4d4055590d
Blaine <rishi1998@gmail.com>
2020-07-04 10:13:27 +0530

    iconify instead of minimize (gtk)

--- webview-sys/gtk.rs
+++ webview-sys/gtk.rs
@@ -59,7 +59,7 @@ unsafe extern "C" fn webview_set_maximized(webview: *mut WebView, maximize: c_in
 
 #[no_mangle]
 unsafe extern "C" fn webview_set_minimized(webview: *mut WebView) {
-    gtk_window_minimized(mem::transmute((*webview).window));
+    gtk_window_iconify(mem::transmute((*webview).window));
 }
 
 #[no_mangle]
